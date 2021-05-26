099c00cb55c9e3de5313321af834c165c337f377
Levi Aho <leviaho88@gmail.com>
2021-05-26 03:43:24 -0700

    Commit for core src files

--- src/main/java/com/levi/j_file/JFileApplication.java
+++ src/main/java/com/levi/j_file/JFileApplication.java
@@ -2,8 +2,14 @@ package com.levi.j_file;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.levi.j_file.property.FileStorageProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+	FileStorageProperties.class
+})
 public class JFileApplication {
 
 	public static void main(String[] args) {
